d26719931da4c82187d2a94f505084d9a79f6c79
Brandon Trecki <treckstar@gmail.com>
2023-12-01 01:02:22 -0500

    fix: tina demo

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,6 +1,6 @@
 import 'css/tailwind.css'
 import 'pliny/search/algolia.css'
-
+import React from 'react'
 import { Space_Grotesk } from 'next/font/google'
 import { Analytics, AnalyticsConfig } from 'pliny/analytics'
 import { SearchProvider, SearchConfig } from 'pliny/search'
--- app/theme-providers.tsx
+++ app/theme-providers.tsx
@@ -1,5 +1,5 @@
 'use client'
-
+import React from 'react'
 import { ThemeProvider } from 'next-themes'
 import siteMetadata from '@/data/siteMetadata'
 
